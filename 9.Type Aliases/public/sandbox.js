54c04a938918aa99df867398e6570d8d7a42c57b
i.mekideche <i.mekideche@anem.dz>
2021-05-09 11:35:47 +0100

    Type Aliases - tsc

--- 9.Type Aliases/public/sandbox.js
+++ 9.Type Aliases/public/sandbox.js
@@ -1,31 +1,7 @@
 "use strict";
-/* let test = () => {
-  console.log("hello world!");
-}; */
-// test = "hello world!"; ==> error
-var test;
-// test = "hello world!"; ==> error
-/*********************************************************************************************************************/
-var add = function (a, b) {
-    console.log(a + b);
+var logDetails = function (uid, item) {
+    console.log(item + " has a uid of " + uid);
 };
-add(5, 20);
-var addNumbers = function (a, b, c) {
-    console.log(a + b);
-    console.log(c);
+var greet = function (user) {
+    console.log(user.name + " says hello !");
 };
-// addNumbers(5, 20); ==> error if no ? with c
-addNumbers(5, 20);
-// Or with a default value
-var addNumber = function (a, b, c) {
-    if (c === void 0) { c = 10; }
-    console.log(a + b);
-    console.log(c);
-};
-addNumber(5, 20, "33");
-/*********************************************************************************************************************/
-var minus = function (a, b) {
-    return a + b;
-};
-var result = minus(25, 33);
-// result = "something else"; ==> error because result is a number not a string
