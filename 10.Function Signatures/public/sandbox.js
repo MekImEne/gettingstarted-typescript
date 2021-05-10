ed9d90d67fe15d291b9829e401d5cdf9a3f05a07
i.mekideche <i.mekideche@anem.dz>
2021-05-10 14:47:24 +0100

    Function Signatures - tsc

--- 10.Function Signatures/public/sandbox.js
+++ 10.Function Signatures/public/sandbox.js
@@ -1,7 +1,22 @@
 "use strict";
-var logDetails = function (uid, item) {
-    console.log(item + " has a uid of " + uid);
+// let greet: Function;
+// example 1
+var greet;
+greet = function (name, greeting) {
+    console.log(name + " says " + greeting);
 };
-var greet = function (user) {
-    console.log(user.name + " says hello !");
+// example 2
+var calc;
+calc = function (numOne, numTwo, action) {
+    if (action === "add") {
+        return numOne + numTwo;
+    }
+    else {
+        return numTwo - numOne;
+    }
+};
+// exampel 3
+var logDetails;
+logDetails = function (mimi) {
+    console.log(mimi.name + " is " + mimi.age + " years old.");
 };
