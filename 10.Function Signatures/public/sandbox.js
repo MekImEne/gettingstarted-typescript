45910522d3936eab72d23e6ac56e2b82f2062c29
i.mekideche <i.mekideche@anem.dz>
2021-05-10 14:45:11 +0100

    Function Signatures

---
+++ 10.Function Signatures/public/sandbox.js
@@ -0,0 +1,7 @@
+"use strict";
+var logDetails = function (uid, item) {
+    console.log(item + " has a uid of " + uid);
+};
+var greet = function (user) {
+    console.log(user.name + " says hello !");
+};
---
+++ 10.Function Signatures/src/sandbox.ts
@@ -0,0 +1,31 @@
+// let greet: Function;
+
+// example 1
+
+let greet: (a: string, b: string) => void;
+
+greet = (name: string, greeting: string) => {
+  console.log(`${name} says ${greeting}`);
+};
+
+// example 2
+
+let calc: (a: number, b: number, c: string) => number;
+
+calc = (numOne: number, numTwo: number, action: string) => {
+  if (action === "add") {
+    return numOne + numTwo;
+  } else {
+    return numTwo - numOne;
+  }
+};
+
+// exampel 3
+
+let logDetails: (obj: { name: string; age: number }) => void;
+
+type person = { name: string; age: number };
+
+logDetails = (mimi: person) => {
+  console.log(`${mimi.name} is ${mimi.age} years old.`);
+};
