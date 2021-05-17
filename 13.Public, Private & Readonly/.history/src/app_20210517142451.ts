dd57cd97b4b11431dd06f4d775fc395f62e82ba0
i.mekideche <i.mekideche@anem.dz>
2021-05-17 14:30:37 +0100

    Public, Private & Readonly

---
+++ 13.Public, Private & Readonly/.history/src/app_20210517142451.ts
@@ -0,0 +1,70 @@
+// Classes
+
+class Invoice {
+  readonly client: string;
+  private details: string;
+  public amount: number;
+
+  constructor(c: string, d: string, a: number) {
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  }
+
+  format() {
+    return `${this.client} owes $${this.amount} for ${this.details}`;
+  }
+}
+
+const invOne = new Invoice(
+  "Imane",
+  "work on getting started with typescript",
+  25
+);
+
+const invTwo = new Invoice(
+  "Ikram",
+  "work on getting started with react native",
+  23
+);
+
+console.log(invOne, invTwo);
+
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
+
+invOne.client = "assem";
+invTwo.client = "imane";
+invTwo.amount = 25;
+invOne.amount = 33;
+
+console.log(invOne, invTwo);
+
+invoices.forEach((inv) => {
+  // console.log(inv.client, inv.details, inv.amount, inv.format()); => error bcz details is private
+  console.log(inv.client, inv.amount, inv.format());
+});
+
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+
+// inputs
+const type = document.querySelector("#type") as HTMLSelectElement;
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
+
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+
+  console.log(
+    type.value,
+    ", ",
+    tofrom.value,
+    ", ",
+    details.value,
+    ", ",
+    amount.valueAsNumber
+  );
+});
---
+++ 13.Public, Private & Readonly/public/app.js
@@ -0,0 +1,45 @@
+"use strict";
+// Classes
+var Invoice = /** @class */ (function () {
+    /* readonly client: string;
+    private details: string;
+    public amount: number; */
+    function Invoice(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+        /* this.client = c;
+        this.details = d;
+        this.amount = a; */
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes $" + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
+var invOne = new Invoice("Imane", "work on getting started with typescript", 25);
+var invTwo = new Invoice("Ikram", "work on getting started with react native", 23);
+console.log(invOne, invTwo);
+var invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
+// invOne.client = "assem"; ==> error cz client is a readonly field
+// invTwo.client = "imane";
+invTwo.amount = 25;
+invOne.amount = 33;
+console.log(invOne, invTwo);
+invoices.forEach(function (inv) {
+    // console.log(inv.client, inv.details, inv.amount, inv.format()); => error bcz details is private
+    console.log(inv.client, inv.amount, inv.format());
+});
+var form = document.querySelector(".new-item-form");
+// inputs
+var type = document.querySelector("#type");
+var tofrom = document.querySelector("#tofrom");
+var details = document.querySelector("#details");
+var amount = document.querySelector("#amount");
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log(type.value, ", ", tofrom.value, ", ", details.value, ", ", amount.valueAsNumber);
+});
